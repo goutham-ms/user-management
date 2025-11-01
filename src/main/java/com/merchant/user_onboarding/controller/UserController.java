16385ed9f0c3604c7bceae11697b962463036a73
goutham-ms <143820624+goutham-ms@users.noreply.github.com>
2025-11-01 15:53:46 +0530

    feat(User): add validations for value object

--- src/main/java/com/merchant/user_onboarding/controller/UserController.java
+++ src/main/java/com/merchant/user_onboarding/controller/UserController.java
@@ -2,15 +2,18 @@ package com.merchant.user_onboarding.controller;
 
 import com.merchant.user_onboarding.service.UserService;
 import com.merchant.user_onboarding.vos.UserVO;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
@@ -22,19 +25,19 @@ public class UserController {
     }
 
     @PostMapping("/create-user")
-    public ResponseEntity<String> createUser(@RequestBody UserVO user) {
+    public ResponseEntity<String> createUser(@Valid @RequestBody UserVO user) {
         userService.addUser(user);
         return new ResponseEntity<>("New User Created!", HttpStatus.OK);
     }
 
     @PutMapping("/update-user")
-    public ResponseEntity<String> updateUser(@RequestBody UserVO user) {
+    public ResponseEntity<String> updateUser(@Valid @RequestBody UserVO user) {
         userService.updateUser(user);
         return new ResponseEntity<>("User Updated!", HttpStatus.OK);
     }
 
     @DeleteMapping("/delete-user")
-    public ResponseEntity<String> deleteUser(@RequestBody UserVO user) {
+    public ResponseEntity<String> deleteUser(@Valid @RequestBody UserVO user) {
         userService.deleteUser(user);
         return new ResponseEntity<>("User Deleted!", HttpStatus.OK);
     }
--- src/main/java/com/merchant/user_onboarding/vos/UserVO.java
+++ src/main/java/com/merchant/user_onboarding/vos/UserVO.java
@@ -1,5 +1,6 @@
 package com.merchant.user_onboarding.vos;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,14 +10,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserVO {
 
+    @NotNull
+    @Size(min=2, max=50, message = "Should be more than 3 and less than 50 character!")
     private String userId;
 
+    @NotBlank(message = "Name cannot be blank!")
+    @Size(min=3, max=20, message = "Should not be more than 20 Characters!")
+    @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]*(?: [a-zA-Z0-9]+)*$", message = "Username must start with a letter and contain only letters and numbers.")
+    @Pattern(regexp = "^([A-Z][a-z]+)(\\s[A-Z][a-z]+){0,2}\\s([A-Z][a-z]+)$", message = "Only first letter of the name should be in capital")
     private String userName;
 
+    @Min(value = 10, message = "Age should be more than 10")
+    @Max(value = 150, message = "Age should not be more than 150")
     private Long age;
 
+    @Pattern(regexp = "(?i)ENGINEERING|MANAGEMENT|SALES", message = "Category must be ENGINEERING, MANAGEMENT or SALES")
     private String department;
 
+    @NotNull(message = "DOB cannot be null")
     private String dateOfBirth;
 
     private String registeredDate;
